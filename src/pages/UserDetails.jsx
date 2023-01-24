da32d56ad1ae37df288f0ed6fde441902e37c092
Havock48 <adit.github@gmail.com>
2023-01-24 16:43:43 +0100

    users profile page created with only user details

--- src/pages/UserDetails.jsx
+++ src/pages/UserDetails.jsx
@@ -1,16 +1,28 @@
 import { useEffect } from "react";
 import { useParams } from "react-router"
 import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar"
 import { BsArrowLeft } from 'react-icons/bs';
+import { db } from "../firebase.config";
+import { doc, getDoc } from "firebase/firestore";
+import { useState } from "react";
+import avatar_image from './images/avatar-image.png'
+import './css/userDetails.css'
 
 export const UserDetails = () => {
-
+    const [ userDetails, setUserDetails ] = useState();
     const params = useParams();
 
     useEffect(() => {
-        console.log(params.user)
+        getUserDetails();
     }, [])
+
+    const getUserDetails = async () => {
+        const userDoc = doc(db, 'users', params.user);
+        const getUserDoc = await getDoc(userDoc);
+        const data = getUserDoc.data();
+
+        setUserDetails(data);
+    }
 
     return (
         <>
@@ -20,6 +32,41 @@ export const UserDetails = () => {
                         <BsArrowLeft />
                     </Link>
                     <h3>Profil korisnika</h3>
+                </div>
+            </div>
+            <div className="userDetails">
+                <div className="container">
+                    {userDetails &&
+                        <div className="user-section">
+                            <h1>{userDetails.reg_name + ' ' + userDetails.reg_lname}</h1>
+                             <div className="user">
+                                <div className="image">
+                                    <img src={avatar_image} alt="" />
+                                </div>
+                                <div className="details">
+                                    <ul>
+                                        <li>
+                                            <p>Lokacija</p>
+                                            <p>{userDetails.reg_city}</p>
+                                        </li>
+                                        <li>
+                                            <p>Adresa</p>
+                                            <p>Nije navedeno</p>
+                                        </li>
+                                        <li>
+                                            <p>Telefon</p>
+                                            <p>{userDetails.reg_number}</p>
+                                        </li>
+                                        <li>
+                                            <p>Tip korisnika</p>
+                                            <p>Korisnik</p>
+                                        </li>
+                                    </ul>
+                                    <button className="btn" onClick={() => alert('Nije u funkciji')}>Pošalji poruku</button>
+                                </div>
+                             </div>
+                        </div>
+                    }
                 </div>
             </div>
         </>
