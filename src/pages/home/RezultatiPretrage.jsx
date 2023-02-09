7d4894ad138fd31774bc65ff6338a13350ec534b
Havock48 <adit.github@gmail.com>
2023-02-09 23:19:28 +0100

    rezultati pretrage for tablet version done

--- src/pages/home/RezultatiPretrage.jsx
+++ src/pages/home/RezultatiPretrage.jsx
@@ -14,29 +14,33 @@ export const RezultatiPretrage = () => {
             <div className="rezultati-pretrage">
                 <div className="container">
                     <h1>Rezultati pretrage: <span>{searchFormData}</span></h1>
-                    {searchData && 
-                        searchData.map(({data: {storageImages, naslov_oglasa, lokacija_vozila, godiste, kilometraza, gorivo, cijena}, id}, index) => (
-                            <Link to={`/${id}`} key={index} id='link'>
-                                <div className="car-item">
-                                <div className="image">
-                                    <img src={storageImages[0]} alt="" />
-                                </div>
-                                <div className="car-info">
-                                    <h3>{naslov_oglasa}</h3>
-                                    <span>{lokacija_vozila}</span>
-                                    <ul>
-                                        <li><FaCalendarAlt /> {godiste}</li>
-                                        <li><FaRoad /> {kilometraza}</li>
-                                        <li><GiGasPump /> {gorivo}</li>
-                                    </ul>
-                                    <div className="price">
-                                        <h2>{cijena} KM</h2>
+                    <div className="cars-container">
+                        <div className="cars">
+                        {searchData && 
+                            searchData.map(({data: {storageImages, naslov_oglasa, lokacija_vozila, godiste, kilometraza, gorivo, cijena}, id}, index) => (
+                                <Link to={`/${id}`} key={index} id='link'>
+                                    <div className="car-item">
+                                    <div className="image">
+                                        <img src={storageImages[0]} alt="" />
+                                    </div>
+                                    <div className="car-info">
+                                        <h3>{naslov_oglasa}</h3>
+                                        <span>{lokacija_vozila}</span>
+                                        <ul>
+                                            <li><FaCalendarAlt /> {godiste}</li>
+                                            <li><FaRoad /> {kilometraza}</li>
+                                            <li><GiGasPump /> {gorivo}</li>
+                                        </ul>
+                                        <div className="price">
+                                            <h2>{cijena} KM</h2>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            </Link>
-                        ))
-                    }
+                                </Link>
+                            ))
+                        }
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
